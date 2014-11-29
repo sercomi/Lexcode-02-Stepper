3df98f01004e22235c5611b7424a4c06c663eff6
Sergi Comas <sercomi@gmail.com>
2014-11-29 13:14:57 +0100

    Initial Commit

---
+++ 02-Stepper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  02-Stepper
+//
+//  Created by Sergi Comas on 29/11/14.
+//  Copyright (c) 2014 sercomi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ 02-StepperTests/_2_StepperTests.swift
@@ -0,0 +1,36 @@
+//
+//  _2_StepperTests.swift
+//  02-StepperTests
+//
+//  Created by Sergi Comas on 29/11/14.
+//  Copyright (c) 2014 sercomi. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class _2_StepperTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
